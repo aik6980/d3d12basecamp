001f0e5600ce56a4e29c1e7abe7afabb6a9374e5
aik6980 <aik6980>
2017-01-26 08:04:05 +0000

    update d3d12 code

--- src/device_d3d12/renderer/d3d12/frame_resource.h
+++ src/device_d3d12/renderer/d3d12/frame_resource.h
@@ -22,5 +22,8 @@ namespace D3D12
 		// Fence value to mark commands up to this fence point.  This lets us
 		// check if these frame resources are still in use by the GPU.
 		UINT64 m_fence = 0;
+
+		// Frame Temporary resources
+
 	};
 }
