8bfbd3eb255a492d506ca529109a4166e20c31f9
aik6980 <aik6980>
2017-02-09 08:12:59 +0000

    removed duplicated files

--- src/device_d3d12/shaders/default.ps.hlsl
+++
@@ -1,7 +0,0 @@
-#include "default_shared.hlsli"
-
-float4 main(PS_INPUT input) : SV_Target0
-{
-	float4 output = float4(input.position.xy, 0, 1);
-	return output;
-}
--- src/device_d3d12/shaders/util.hlsli
+++
@@ -1,6 +0,0 @@
-
-
-float4 get_value(float a)
-{
-	return float4(a, 1, 1, 1);
-}
